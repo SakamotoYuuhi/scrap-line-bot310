0e8873d9a2246087f94e4993da4760e25cf6d22c
YuuhiSakamoto <neipia_number@icloud.com>
2021-06-01 22:24:56 +0900

    test

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ def Select_Url(prefecture):
 
     if prefecture == '福岡':
         load_url += 'fukuoka_tenjin/'
-    elif prefecture == '岡山':
+    elif prefecture == '岡山': # 無反応
         load_url += 'okayama/'
-    elif prefecture == '大阪':
+    elif prefecture == '大阪': # 無反応
         load_url += 'osaka_nazobldg/'
     elif prefecture == '京都':
         load_url += 'kyoto/'
@@ -49,55 +49,57 @@ def Select_Url(prefecture):
         load_url += 'asakusa/'
     elif prefecture == '東新宿':
         load_url += 'shinjuku/'
-    elif prefecture == '仙台':
+    elif prefecture == '仙台': # 不要な英単語
         load_url += 'sendai/'
-    elif prefecture == '札幌':
+    elif prefecture == '札幌': # 不要な英単語
         load_url += 'sapporo/'
     # 該当する地名がなければシステム自体が終了
     else:
         sys.exit()
 
+    print('OK')
     return load_url
 
 def ScrapInfo(prefecture, load_url):
-  # Webページを取得して解析
-  html = requests.get(load_url)
-  soup = BeautifulSoup(html.content, 'html.parser')
+    # Webページを取得して解析
+    html = requests.get(load_url)
+    soup = BeautifulSoup(html.content, 'html.parser')
 
-  detail_url = soup.find_all(class_='asset-more-link')
-  detail_url_list = []
-  for url in detail_url:
-      detail_url_list.append(url.find('a').get('href'))
+    detail_url = soup.find_all(class_='asset-more-link')
+    detail_url_list = []
+    for url in detail_url:
+        detail_url_list.append(url.find('a').get('href'))
 
-  event_info = soup.find_all(class_='post-content text_area')
-  event_info = [s for s in event_info if prefecture in  s.text]
-  event_info_list = []
-  for i, element in enumerate(event_info):
-      if i == 0:
-          event_info_list.append('------------------------------\n')
-      info_list = element.text.split('\n')
-      info_list = [s for s in info_list if s != '']
-      info_list.append(detail_url_list[i])
-      
-      info_list.pop(0)
-      
-      play_form = 'プレイ形式'
-      n = info_list.index(play_form)
-      info_list[n:n] = '\n'
-      
-      place = '会場'
-      n = info_list.index(place)
-      info_list[n:n+2] = '\n'
-    
-      date = '開催日程'
-      n = info_list.index(date)
-      info_list[n:n] = ''
-      
-      info_list.append('\n------------------------------\n')
-      event_info_list.append('\n'.join(info_list))
-      
-  event_info_text = ''.join(event_info_list)
-  return event_info_text 
+    event_info = soup.find_all(class_='post-content text_area')
+    event_info = [s for s in event_info if prefecture in  s.text]
+    event_info_list = []
+    for i, element in enumerate(event_info):
+        if i == 0:
+            event_info_list.append('------------------------------\n')
+        info_list = element.text.split('\n')
+        info_list = [s for s in info_list if s != '']
+        info_list.append(detail_url_list[i])
+        
+        info_list.pop(0)
+        
+        play_form = 'プレイ形式'
+        n = info_list.index(play_form)
+        info_list[n:n] = '\n'
+        
+        place = '会場'
+        n = info_list.index(place)
+        info_list[n:n+2] = '\n'
+        
+        date = '開催日程'
+        n = info_list.index(date)
+        info_list[n:n] = ''
+        
+        info_list.append('\n------------------------------\n')
+        event_info_list.append('\n'.join(info_list))
+        
+    event_info_text = ''.join(event_info_list)
+    print('OK2')
+    return event_info_text 
 
 @app.route('/')
 def test():
