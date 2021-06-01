e0238835954d6979181d7ad22aea5e4c6808be42
YuuhiSakamoto <neipia_number@icloud.com>
2021-06-01 19:51:27 +0900

    test

--- app.py
+++ app.py
@@ -87,10 +87,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
   event_info_text = ScrapInfo(event.message.text)
-  event_info_text += event.message.text
   line_bot_api.reply_message(
       event.reply_token,
-      TextSendMessage(text=event.message.text))
+      TextSendMessage(text=event_info_text))
 
 
 if __name__ == "__main__":
